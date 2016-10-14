4742249abcf9c7bbc4ca6f493a53179136179b8b
A-Kod <andrey_kodyk@mail.ru>
2016-10-14 20:05:21 +0300

    first commit

---
+++ include/print_hello_world.h
@@ -0,0 +1,8 @@
+//
+// Created by Kodyk Valentina on 14.10.16.
+//
+
+#ifndef LAB_PRINT_HELLO_WORLD_H_H
+#define LAB_PRINT_HELLO_WORLD_H_H
+void print_hello_world();
+#endif //LAB_PRINT_HELLO_WORLD_H_H
---
+++ sources/print_hello_world.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Kodyk Valentina on 14.10.16.
+//
+
+#include <print_hello_world.h>
+#include <iostream>
+
+void print_hello_world()
+{
+    std::cout<<"Hello world!"<<std::endl;
+}
